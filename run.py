9846d722c224250abd639d91c531f3e4bd6153f0
Fisboll <fredrik96hansen@gmail.com>
2022-12-15 20:28:59 +0000

    fixed python code

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
-from cgitb import reset
-from curses.ascii import isalpha
-from doctest import COMPARISON_FLAGS
 import random
-from one_word import word_list
-import controllers
+from one_word import word_list as word_list_one
+
 
 def get_word():
     """
     Fetches random word from words.py and returns it in capital letters
     """
-    word = random.choice(word_list)
+    word = random.choice(word_list_one)
     return word.upper()
-
