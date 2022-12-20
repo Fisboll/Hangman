df68b017fb068f6b3641ccb99cb8ebb4e0e31ed9
Fisboll <fredrik96hansen@gmail.com>
2022-12-20 20:56:59 +0000

    Fixed some minor bugs with mentors help. Added self into the system and added the Two-word python list.

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from two_word import word_list as word_list_two
 
 
 class text_colors:
+    """
+    adds different color to wining and end screen and intro.
+    """
     BLUE = '\033[38;5;159m'
     GREEN = '\033[92m'
     RED = '\033[91m'
@@ -11,42 +14,45 @@ class text_colors:
 
 
 class game_settings:
+    """
+    Sets the game's lives and word count
+    """
     def __init__(self, lives, words):
         self.lives = lives
-        self.word = words
-        self.hidden = ''
+        self.hidden = ""
+        if words == "one":
+            self.get_word()
+        else:
+            self.get_word_two()
+
+    def get_word(self):
+        """
+        Fetches random word from one_word.py and returns it in capital letters
+        """
+        self.word = random.choice(word_list_one).upper()
+        self.hidden = "_" * len(self.word)
+
+    def get_word_two(self):
+        """
+        Fetches random word from two_word.py and returns it
+        """
+        word = random.choice(word_list_two).upper()
+        choosen_word = word.split("-")
+        print(choosen_word)
+        first_hidden = "_" * len(choosen_word[0])
+        second_hidden = "_" * len(choosen_word[1])
+        print(first_hidden, second_hidden)
+        self.word = choosen_word[0] + " " + choosen_word[1]
+        self.hidden = first_hidden + " " + second_hidden
 
 
-def get_word(self):
+def intro_logo():
     """
-    Fetches random word from one_word.py and returns it in capital letters
+    intro Logo. Shows up when you start the game.
     """
-    self.word = random.choice(word_list_one).upper()
-    self.hidden = "_" + len(self.word)
-
-
-def get_word_two(self):
-    word = random.choice(word_list_two).upper()
-    bob = word.split('-')
-    print(bob)
-    first_hidden = "_" * len(bob[0])
-    second_hidden = "_" * len(bob[1])
-    print(first_hidden, second_hidden)
-    self.word = bob[0] + '' + bob[1]
-    self.hidden = first_hidden + '' + second_hidden
-
-
-def intro(word):
-    """
-    displaying word for each turn, will run until
-    user guesses word or runs out of tries
-    """
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 8
-    print(text_colors.BLUE + """ Welcome to
+    print(
+        text_colors.BLUE
+        + """ Welcome to
  _______                       _
 (  _____)                     | |
 | /      ___  ____   ___  ___ | | _____
@@ -61,30 +67,23 @@ def intro(word):
 | | | / ___ | | | ( (_| | | | / ___ | | | |
 |_| |_\_____|_| |_|\___ |_|_|_\_____|_| |_|
                   (_____|
-""" + text_colors.WHITE)
+"""
+        + text_colors.WHITE
+    )
 
-    difficulty_selected = False
-    while difficulty_selected is False:
-        difficulty = input("""Please select difficulty:
-E = Easy, M = Medium, H = Hard: VH = Very Hard """).upper()
-        if difficulty == "E":
-            tries = 8
-            print("You chose Easy difficulty. You have ", tries, "tries.")
-            difficulty_selected = True
-        elif difficulty == "M":
-            tries = 6
-            print("You chose Medium difficulty. You have ", tries, "tries")
-            difficulty_selected = True
-        elif difficulty == "H":
-            tries = 8
-            print("You chose Hard difficulty. You have ", tries, "tries")
-            difficulty_selected = True
-        elif difficulty == "VH":
-            tries = 6
-            print("You chose Very Hard difficulty. You have ", tries, "tries")
-            difficulty_selected = True
-        else:
-            print(difficulty, "is not a difficulty")
+
+def play(game_settings):
+    """
+    displaying word for each turn, will run until
+    user guesses word or runs out of tries
+    """
+    word_completion = game_settings.hidden
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = game_settings.lives
+
+    print(game_settings.word)
 
     print(display_hangman(tries))
     print(word_completion)
@@ -94,7 +93,7 @@ E = Easy, M = Medium, H = Hard: VH = Very Hard """).upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("Whoops! You have already guessed the letter", guess)
-            elif guess not in word:
+            elif guess not in game_settings.word:
                 print(guess, "is not in the word! Try again!")
                 tries -= 1
                 guessed_letters.append(guess)
@@ -103,41 +102,46 @@ E = Easy, M = Medium, H = Hard: VH = Very Hard """).upper()
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
                 indices = [
-                    i for i, letter in enumerate(word)
-                    if letter == guess]
+                i for i, letter in enumerate(game_settings.word) if letter == guess
+                ]
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
                 if "_" not in word_completion:
                     guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
+        elif len(guess) == len(game_settings.word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guesed the word", guess)
-            elif guess != word:
+            elif guess != game_settings.word:
                 print(guess, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
-        elif len(guess) != len(word):
+        elif len(guess) != len(game_settings.word):
             print("Your guess does not have the right amount of letters!")
         else:
             guessed = True
-            word_completion = word
+            word_completion = game_settings.word
 
         print("Woops! Please enter a letter or word! :)")
         print(display_hangman(tries))
         print(word_completion)
-        print(f"There are {len(word)} letters in this word!")
+        print(f"There are {len(game_settings.word)} letters in this word!")
         print("\n")
     if guessed:
         win_game()
     else:
         print(
-            "Sorry, you ran out of tries. The word was " + word +
-            "\n Maybe next time!")
+            "Sorry, you ran out of tries. The word was "
+            + game_settings.word
+            + "\n Maybe next time!"
+        )
         lose_game()
 
 
 def display_hangman(tries):
+    """
+    Display Hangman figures depending on lives.
+    """
     stages = [  # final state: head, torso, both arms, and both legs
                 """
                     --------
@@ -233,9 +237,11 @@ def display_hangman(tries):
 
 def win_game():
     """
-    Shows up when you win the game.
+    Displays when the user wins the game
     """
-    print(text_colors.GREEN + """Congrats, you guessed the word!
+    print(
+        text_colors.GREEN
+        + """Congrats, you guessed the word!
  __    __    ___  _      _          ___     ___   ____     ___
 |  |__|  |  /  _]| |    | |        |   \   /   \ |    \   /  _]
 |  |  |  | /  [_ | |    | |        |    \ |     ||  _  | /  [_
@@ -250,15 +256,19 @@ def win_game():
 |___, ||     ||  :  |    |  `  '  | |  | |  |  | __
 |     ||     ||     |     \      /  |  | |  |  ||  |
 |____/  \___/  \__,_|      \_/\_/  |____||__|__||__|
-""" + text_colors.WHITE)
+"""
+        + text_colors.WHITE
+    )
 
 
 def lose_game():
     """
-    Shows up when you're losing the game.
+    Displays when the user loses the game
     """
-    print(text_colors.RED + """
-
+    print(
+        text_colors.RED
+        + """
+                          
     ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███
    ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀    ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒
   ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒
@@ -268,23 +278,62 @@ def lose_game():
    ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░     ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░
    ░ ░   ░   ░   ▒   ░      ░      ░      ░ ░ ░ ▒       ░░     ░     ░░   ░
     ░       ░  ░       ░      ░  ░       ░ ░        ░     ░  ░   ░
-                                                      ░""" + text_colors.WHITE)
+                                                      ░
+                """
+        + text_colors.WHITE
+    )
 
 
 def select_difficulty():
 
+    difficulty_selected = False
+    while difficulty_selected is False:
+        difficulty = input(
+            """Please select difficulty:
+E = Easy, M = Medium, H = Hard: VH = Very Hard """
+        ).upper()
+        if difficulty == "E":
+            tries = 8
+            print("You chose Easy difficulty. You have ", tries, "tries.")
+            difficulty_selected = True
+            player = game_settings(8, "one")
+            return player
+        elif difficulty == "M":
+            tries = 6
+            print("You chose Medium difficulty. You have ", tries, "tries")
+            difficulty_selected = True
+            player = game_settings(6, "one")
+            return player
+        elif difficulty == "H":
+            tries = 8
+            print("You chose Hard difficulty. You have ", tries, "tries")
+            difficulty_selected = True
+            player = game_settings(8, "two")
+            return player
+        elif difficulty == "VH":
+            tries = 6
+            print("You chose Very Hard difficulty. You have ", tries, "tries")
+            difficulty_selected = True
+            player = game_settings(6, "two")
+            return player
+        else:
+            print(difficulty, "is not a difficulty")
 
-    def main():
- #    """
-  #  Resets the game whenever you win or loses by pressing Y or N.
- #   """
- #   word = get_word()
- #  play(word)
-  #  while input("Play Again? Enter 'Y' for YES"
- #               "\n or any other letter for NO ").upper() == "Y":
-  #      word = get_word()
- #       play(word)
+
+def main():
+
+    intro_logo()
+    game_instance = select_difficulty()
+
+    print(game_instance.lives, game_instance.hidden)
+    play(game_instance)
+    while (
+        input("Play Again? Enter 'Y' for YES" "\n or any other letter for NO ").upper()
+        == "Y"
+    ):
+        game_instance = select_difficulty()
+        play(game_instance)
 
 
-        if __name__ == "__main__":
-            main()
+if __name__ == "__main__":
+    main()
